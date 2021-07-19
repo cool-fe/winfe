7f86b8810c5b3a4f0ab0f7ae68f912daafbe8bb1
dashixiong <876337269@qq.com>
2021-07-19 17:36:08 +0800

    chore: transform js to ts

---
+++ packages/win-dll/src/index.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: smallalso<hu141418@gmail.com>
+ * @Date: 2021-03-04 18:07:23
+ * @LastEditors: smallalso<hu141418@gmail.com>
+ * @LastEditTime: 2021-03-10 17:04:19
+ * @FilePath: /winning-webcomponents-finance-common/src/his-dll/index.js
+ */
+import WinningDll from './core/index';
+
+export default WinningDll;
--- packages/win-dll/src/util/cookieData.ts
+++ packages/win-dll/src/util/cookieData.ts
@@ -7,21 +7,21 @@
  * @FilePath: /winning-webcomponents-finance-common/src/utils/cookieData.js
  */
 
-import Cookie from 'js-cookie'
+import Cookie from 'js-cookie';
 
 const cookieExternals = {
   userInfo: true,
   BEARER_TOKEN: true,
   usersInfoId: true
-}
+};
 
 /**
  * 根据key获取指定cookie
  * @param {String} key
  * @returns
  */
-function getCookieByKey (key) {
-  return Cookie.get(key)
+function getCookieByKey(key: string): string {
+  return Cookie.get(key) || '';
 }
 
 /**
@@ -30,49 +30,52 @@ function getCookieByKey (key) {
  * @param {String} value 当前key的cookie值，如果传入则不再去cookie中拿
  * @returns
  */
-function getCookieByKeyInDefault (key, value) {
-  const data = value || getCookieByKey(key)
+// eslint-disable-next-line complexity
+function getCookieByKeyInDefault(key: string, value?: string | undefined): any {
+  const data = value || getCookieByKey(key);
   // const data = copydata ? encodeURI(copydata).replace(/%20/g, ' ') : copydata
   switch (key) {
     case 'W-SEQ':
-      return data || '1569595974015_2'
+      return data || '1569595974015_2';
     case 'W-FLOW':
-      return data || 'default'
+      return data || 'default';
     case 'X-DEBUG' || 'W-EVENT':
-      return data || null
+      return data || null;
     case 'Authorization':
-      return getCookieByKeyInDefault('BEARER_TOKEN')
+      return getCookieByKeyInDefault('BEARER_TOKEN');
     case 'BEARER_TOKEN':
-      return data
+      return data;
     case 'ip':
-      return data || 'http://127.0.0.1'
+      return data || 'http://127.0.0.1';
     default:
-      return data ? encodeURI(data).replace(/%20/g, ' ') : data
+      return data ? encodeURI(data).replace(/%20/g, ' ') : data;
   }
 }
 
 /**
  * 获取cookieData
  */
-function getCookieData () {
-  const cookieData = {}
-  const cookie = Cookie.get()
+function getCookieData(): any {
+  const cookieData: any = {};
+  const cookie = Cookie.get();
   // 遍历所有key
+  // eslint-disable-next-line no-restricted-syntax
   for (const key in cookie) {
     if (Object.hasOwnProperty.call(cookie, key)) {
       if (cookieExternals[key]) {
-        continue
+        // eslint-disable-next-line no-continue
+        continue;
       }
-      cookieData[key] = getCookieByKeyInDefault(key, cookie[key])
+      cookieData[key] = getCookieByKeyInDefault(key, cookie[key]);
     }
   }
 
   // 拼装Authorization
-  cookieData.Authorization = getCookieByKeyInDefault('Authorization')
+  cookieData.Authorization = getCookieByKeyInDefault('Authorization');
 
-  return cookieData
+  return cookieData;
 }
 
-export default getCookieData()
+export default getCookieData();
 
-export { getCookieByKeyInDefault, getCookieData, getCookieByKey }
+export { getCookieByKeyInDefault, getCookieData, getCookieByKey };
